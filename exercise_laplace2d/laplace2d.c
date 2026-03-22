3517aeabe10b453842265d22fe1178cdfbbe3bea
Giovana Fernandes <gigiaero@gigi-debian.gigi.net>
2026-03-22 16:03:54 -0300

    result saving and log saving are now in dedicated functions

--- exercise_laplace2d/laplace2d.c
+++ exercise_laplace2d/laplace2d.c
@@ -35,12 +35,48 @@ void N_p_jacobi(double *N,double *x,double *y,int i,int j){
 - gigiaero, 22/03/2026, 0958 hours
 */
 void calc_residual(double *phi_elem,double *phi_old_elem,double *N_elem,
-                   double *res_elem,double *val){
+                   double *res_elem){
 
-  *val = fabs((*N_elem)*((*phi_elem) - (*phi_old_elem)));
+  double val;
+  val = fabs((*N_elem)*((*phi_elem) - (*phi_old_elem)));
 
-  if(*val > *res_elem)
-    *res_elem = *val;
+  if(val > *res_elem)
+    *res_elem = val;
+}
+
+/*
+- gigiaero, 22/03/2026, 1034 hours
+*/
+void save_mesh(int m,int n,double *x,double *y,char *casename){
+  char *filename = malloc(sizeof(char)*200);
+  int str_end_idx;
+
+  find_str_end(filename,&str_end_idx);
+
+  strcat(filename,casename);
+  find_str_end(filename,&str_end_idx);
+  strcat(filename,"_mesh_x.msh");
+  print_1d_array_to_file(n,x,filename);
+  filename[str_end_idx] = '\0';
+  strcat(filename,"_mesh_y.msh");
+  print_1d_array_to_file(m,y,filename);
+
+  free(filename);
+
+}
+
+/*
+- gigiaero, 22/03/2026, 1121 hours
+*/
+void save_results_qtimes(int m,int n,double phi[m][n],int *iter,int *qtimes,
+                         char *buffer,char *filename_save,int *str_end_idx){
+  if(*iter%(*qtimes) == 0){
+    sprintf(buffer,"%010d",*iter);
+    strcat(filename_save,buffer);
+    strcat(filename_save,".dat");
+    print_2d_array_to_file(m,n,phi,filename_save,1);
+    filename_save[*str_end_idx] = '\0'; // reset string to replace iter number
+  }
 }
 
 /*
@@ -59,30 +95,23 @@ void solve_p_jacobi_2d_rectangular(int m,int n,double phi[m][n],double *x,
   // Residuals
   char *filename_log = malloc(sizeof(char)*200);
   double *res = calloc(config->max_iter,sizeof(double));
-  double val;
+  // double val;
   FILE *file_log;
 
   // Configure log file
   strcat(filename_log,config->casename);
   strcat(filename_log,".log");
   // Reset it, if exists, then reopen
-  puts(filename_log);
   file_log = fopen(filename_log,"w");
   fclose(file_log);
   file_log = fopen(filename_log,"a");
 
   // Save mesh
+  save_mesh(m,n,x,y,config->casename);
   strcat(filename_save,config->casename);
-  find_str_end(filename_save,&str_end_idx);
-  strcat(filename_save,"_mesh_x.msh");
-  print_1d_array_to_file(n,x,filename_save);
-  filename_save[str_end_idx] = '\0';
-  strcat(filename_save,"_mesh_y.msh");
-  print_1d_array_to_file(m,y,filename_save);
 
   // Prepare string to save simulation data  
-  filename_save[str_end_idx] = '\0';
-  strcat(filename_save,"_iter");
+  strcat(filename_save,"_iter_");
   find_str_end(filename_save,&str_end_idx);
 
   for(int j=1;j<m-1;j++){
@@ -97,20 +126,13 @@ void solve_p_jacobi_2d_rectangular(int m,int n,double phi[m][n],double *x,
       for(int i=1;i<n-1;i++){
         scheme_der2_o2_central_var_deltas_xy(&L_phi,m,n,phi,x,y,i,j);
         phi[j][i] = -L_phi/N[j][i] + phi_old[j][i];
-
-        calc_residual(&phi[j][i],&phi_old[j][i],&N[j][i],&res[iter],&val);
+        calc_residual(&phi[j][i],&phi_old[j][i],&N[j][i],&res[iter]);
       }
     }
 
-    printf("Iteration %d | Res %.6e\n",iter,res[iter]);
+    printf("Iteration %010d | Res %.6e\n",iter,res[iter]);
 
-    if(iter%config->qtimes == 0){
-      itoa(iter,buffer,10);
-      strcat(filename_save,buffer);
-      strcat(filename_save,".dat");
-      print_2d_array_to_file(m,n,phi,filename_save,1);
-      filename_save[str_end_idx] = '\0'; // reset string to replace iter
-    }
+    save_results_qtimes(m,n,phi,&iter,&(config->qtimes),buffer,filename_save,&str_end_idx);
 
     fprintf(file_log,"%.6e\n",res[iter]);
 
@@ -172,7 +194,7 @@ int main(){
   // Solution configurations
   sim_parameters config;
   config.Ntype = 1;
-  config.max_iter = 5000;
+  config.max_iter = 50;
   config.qtimes = 10;
   config.eps = 1.e-5; // Convergence criterion
   char output_file[] = "results/laplace2d";
