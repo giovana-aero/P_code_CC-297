4af3f1d79102962e689a6a3a183ffc2cd47a361c
Giovana Fernandes <gigiaero@gigi-debian.gigi.net>
2026-03-24 17:46:21 -0300

    lgs implementation and tests in laplace2d

--- include/num_methods.h
+++ include/num_methods.h
@@ -18,14 +18,13 @@ typedef struct s_parameters{
 void calc_residual(double *phi_elem,double *phi_old_elem,double *N_elem,
                   double *res_elem);
 int check_num_digits_int(int *num);
+double d_yy(int m,int n,double A[m][n],double *y,int i,int j);
 double delta_xy(double *xy,int i);
 void diagonal_matrix_solver(int n,double A[n][n],double *f,double *u);
 void evaluate_delta_form(int m,int n,double phi[m][n],double *x,double *y,
                          sim_parameters *config,int num_b_c_r,
                          b_conditions_2d *b_c_r);
 void N_p_jacobi(double *N,double *x,double *y,int i,int j);
-// void save_results_qtimes(int m,int n,double phi[m][n],int *iter,int *qtimes,
-//                          char *buffer,char *filename_save,int *str_end_idx);
 void save_results_qtimes(int m,int n,double phi[m][n],int *iter,
                          sim_parameters *s_p,char *buffer,char *filename_save,
                          int *str_end_idx);
@@ -37,6 +36,9 @@ void scheme_der2_o2_central_var_deltas_xy(double *f,int m,int n,
 void solve_g_seidel_2d_rectangular(int m,int n,double phi[m][n],double *x,
                                    double *y,sim_parameters *config,
                                    int num_b_c_r,b_conditions_2d *b_c_r);
+void solve_lgs_2d_rectangular(int m,int n,double phi[m][n],double *x,double *y,
+                              sim_parameters *config,
+                              int num_b_c_r,b_conditions_2d *b_c_r);
 void solve_p_jacobi_2d_rectangular(int m,int n,double phi[m][n],double *x,
                                    double *y,sim_parameters *config,
                                    int num_b_c_r,b_conditions_2d *b_c_r);
